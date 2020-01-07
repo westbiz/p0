fe90b634e0975bf0099eda944b8d03cde44b4dd1
westbiz <westiz@163.com>
2020-01-07 23:52:49 +0800

    chinaarea

--- app/Http/Controllers/ChinaAreaController.php
+++ app/Http/Controllers/ChinaAreaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\ChinaArea;
+use Illuminate\Support\Facades\DB;
 
 class ChinaAreaController extends Controller
 {
@@ -87,9 +88,35 @@ class ChinaAreaController extends Controller
     public function getcitiesbyprovince(Request $request)
     {
         $q = $request->get('q');
-        $data = ChinaArea::where('areaName','like',"%$q%")
 
-                ->paginate(null, ['id', 'areaName as text']);
+        $prvlike = ChinaArea::where('areaName','like',"%$q%")
+                            ->where('level',1)->get();
+
+        $regionlike = ChinaArea::where('areaName','like',"%$q%")
+                            ->where('level',2)->get();
+        $citylike = ChinaArea::where('areaName','like',"%$q%")
+                            ->where('level',3)->get();        
+        if ($citylike->isNotEmpty()) {
+            $data = DB::table('t_areas as a')
+                        ->leftjoin('t_areas as p','a.parent_id','=','p.id')
+                        ->select(['a.id', DB::Raw('concat(p.areaName," 》",a.areaName) as text')])
+                        ->where('a.areaName','like',"%$q%")
+                            ->paginate();
+        } elseif ($regionlike->isNotEmpty()) {
+            $data = ChinaArea::where('areaName','like',"%$q%")
+                            ->paginate(null, ['id', 'areaName as text']);
+        } else {
+            $data = DB::table('t_areas as a')
+                        ->rightjoin('t_areas as p','a.id','=','p.parent_id')
+                        ->select(['p.id', DB::Raw('concat(a.areaName," 》",p.areaName) as text')])
+                        ->where('a.areaName','like',"%$q%")
+                            ->paginate();
+        }
+        
+        // dd($citylike->isNotEmpty());
+        // $data = ChinaArea::where('areaName','like',"%$q%")
+
+        //         ->paginate(null, ['id', 'areaName as text']);
         return $data;
     }
 
